fcaf45203ca6418d14df6eba8ea2970607a40d06
Jason Pitteman <pittemanjason@hotmail.com>
2020-10-24 20:31:16 +0200

    Small changes

--- OSD/NumberSetting.py
+++ OSD/NumberSetting.py
@@ -7,15 +7,16 @@ class NumberSetting(Setting):
 
     def __init__(self, name):
         super().__init__(name)
-        self.value: float = None
-        self.unit: str = None
-        self.step: float = None
-        self.minimum: float = None
-        self.maximum: float = None
+        self.value: float = 1
+        self.unit: str = ""
+        self.step: float = 1
+        self.decimals: int = 1
+        self.minimum: float = 0
+        self.maximum: float = 10
 
     def show(self, image):
-        color = (0, 0, 255, 255)
-        cv.putText(image, "{:.1f}".format(self.value) + self.unit, (0,50) , cv.FONT_HERSHEY_SIMPLEX, 1, color, 1)
+        color = (255, 0, 0)
+        cv.putText(image, ("{:." + str(self.decimals) + "f}").format(round(self.value, self.decimals)) + self.unit, (0,50) , cv.FONT_HERSHEY_SIMPLEX, 1, color, 1)
 
     def edit(self, input: ButtonInput, token: str) -> str:
         if input == ButtonInput.OK:
