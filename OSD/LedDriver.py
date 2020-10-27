e5319e0c8913be0e465f90936ddadec374277a1d
Jason Pitteman <pittemanjason@hotmail.com>
2020-10-27 15:36:03 +0100

    Update LedDriver.py

--- OSD/LedDriver.py
+++ OSD/LedDriver.py
@@ -7,23 +7,27 @@ class LedDriver(object):
         GPIO.setmode(GPIO.BCM)
 
         GPIO.setup(r, GPIO.OUT)
-        self.r = GPIO.PWM(r, 1000)
-        self.r.start(100)
+        self.r = GPIO.PWM(r, 255)
+        self.r.start(0)
 
         GPIO.setup(g, GPIO.OUT)
-        self.g = GPIO.PWM(g, 1000)
-        self.g.start(100)
+        self.g = GPIO.PWM(g, 255)
+        self.g.start(0)
 
         GPIO.setup(b, GPIO.OUT)
-        self.b = GPIO.PWM(b, 1000)
-        self.b.start(100)
+        self.b = GPIO.PWM(b, 255)
+        self.b.start(255)
 
         
 
     def output(self, red, green, blue, brightness):
         print("R: " + str(red) + ", G: " + str(green) + ", B: " + str(blue) + " at " + str(brightness) + "%")
+        self.r.ChangeDutyCycle(red)
+        self.g.ChangeDutyCycle(green)
+        self.b.ChangeDutyCycle(blue)
 
     def stop():
         self.r.stop()
         self.g.stop()
         self.b.stop()
+        GPIO.cleanup()
