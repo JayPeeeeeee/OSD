e6055b927a825f77d33c7ef6ea6def9d9add59f8
Jason Pitteman <pittemanjason@hotmail.com>
2020-10-28 10:21:06 +0100

    Update OSD.py

--- OSD/OSD.py
+++ OSD/OSD.py
@@ -18,6 +18,8 @@ from InputManager import InputManager
 from LedDriver import LedDriver
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from Lepton import Lepton
+import numpy as np
 
 settingsFile = "..\OSD Settings.json"
 settings = None
@@ -29,6 +31,21 @@ editToken = None
 buttonInput = None
 inputManager = InputManager(5, 6, 13)
 ledDriver = LedDriver(17, 27, 22)
+faceDet = cv2.CascadeClassifier("/home/pi//SACLeptonRPi/haarcascade_frontalface_default.xml")
+l = Lepton()
+
+
+# Target screen is 12", 1024x768 or 768x1024 in portrait mode
+screenWidth = 768
+screenHeight = 1024
+# Sensor size is 80x60
+sensorWidth = 80
+sensorHeight = 60
+# Lepton offset in degC
+corrVal = 0
+maxVal = 0
+feverThresh = 35.4
+
 
 def readInput():
     global inputManager
@@ -266,6 +283,87 @@ def editSetting(setting, frame):
 
     return False
 
+def measureTemperature(image):
+    global sensorWidth, sensorHeight, maxVal
+
+    runningAvg = 0
+    thSampleCount = 0
+    thSampleAcc = []
+    thDataValid = False
+    nThSamplesToAverage = 6
+
+    gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    rects = faceDet.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(180,180))
+    faceBoxes = [(y,x+w, y+h, x) for (x,y,w,h) in rects]
+    if len(faceBoxes) > 0:
+        tcFace1 = faceBoxes[0] # true color ROI
+        # transform the coordinates from true color image space to thermal image space using the affine transform matrix M
+        # See https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
+        M = np.array([[1.5689e-1, 8.6462e-3, -1.1660e+1],[1.0613e-4, 1.6609e-1, -1.4066e+1]])
+        P_slt = np.array([[tcFace1[3]],[tcFace1[0]],[1]]) # 'slt' source left top
+        P_srt = np.array([[tcFace1[1]],[tcFace1[0]],[1]]) # 'srt' source right top
+        P_slb = np.array([[tcFace1[3]],[tcFace1[2]],[1]]) # 'slb' source left bottom
+        P_srb = np.array([[tcFace1[1]],[tcFace1[2]],[1]]) # 'srb' source right bottom
+        P_dlt = np.dot(M, P_slt)
+        P_drt = np.dot(M, P_srt)
+        P_dlb = np.dot(M, P_slb)
+        P_drb = np.dot(M, P_srb)
+        thFace1Cnts = np.array([P_dlt, P_drt, P_dlb, P_drb], dtype=np.float32)
+        for (top,right,bottom,left) in faceBoxes:
+            cv.rectangle(image,(left,top),(right,bottom), (100,255,100), 1)
+
+    # get thermal image from Lepton
+    raw,_ = l.capture()
+    # find maximum value in raw lepton data array in thRoi (face1) slice of raw data.
+    if len(faceBoxes) > 0:
+        thRoi = cv.boundingRect(thFace1Cnts)
+        x,y,w,h = thRoi
+        # x any shold not be negative. Clip the values.
+        x = max(0, min(x, sensorWidth))
+        y = max(0, min(y, sensorHeight))
+        thRoiData = raw[y:y+h, x:x+w]
+        maxVal = np.amax(thRoiData)
+	    # get running average over N thermal samples
+        if (thSampleCount < nThSamplesToAverage):
+            thSampleCount += 1
+            thSampleAcc.append(maxVal)
+        else:
+            thDataValid = True
+            thSampleAcc.append(maxVal)
+            thSampleAcc.pop(0)
+            runningAvg = sum(thSampleAcc)/len(thSampleAcc)
+            maxCoord = np.where(thRoiData == maxVal)
+    else:
+	    # No faces found.
+        runningAvg = 0
+        thSampleCount = 0
+        del thSampleAcc[:]
+        thDataValid = False
+
+    # text position
+    txtPosition = (500,50)
+
+    cv.normalize(raw, raw, 0, 65535, cv.NORM_MINMAX) # extend contrast
+    np.right_shift(raw, 8, raw) # fit data into 8 bits
+    # draw roi if any
+    if len(faceBoxes) > 0:
+            x,y,w,h = thRoi
+            cv.rectangle(raw, (x,y), (x+w,y+h), 255, 1)
+    # make uint8 image
+    thermal = np.uint8(raw)
+    # convert grayscale to BGR
+    thermal = cv.cvtColor(thermal, cv.COLOR_GRAY2BGR)
+    color = image
+
+    # Put data on top of the image if a face was detected.
+    if len(faceBoxes) == 1 and thDataValid:
+#            measTemp = (float(maxVal/100.0)-273.15) + corrVal
+        measTemp = (float(runningAvg/100.0)-273.15)
+        if measTemp > feverThresh:
+            cv.putText(color, "{}degC".format(measTemp), txtPosition, cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255,255), 2)
+        else:
+            cv.putText(color, "{}degC".format(measTemp), txtPosition, cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0,255),2)
+
 def startDisplay():
     call(["../../SACLeptonRPi/SACDisplayMixer/OGLESSimpleImageWithIPC"])
    
@@ -300,6 +398,7 @@ for data in camera.capture_continuous(rawCapture, format="rgb", use_video_port=T
             initMenus()
         else:
             # Measure temp
+            measureTemperature(frame)
             temp = random.randint(33, 38)
             brightness = settings.brightness.value
             if temp > settings.threshold.value:
