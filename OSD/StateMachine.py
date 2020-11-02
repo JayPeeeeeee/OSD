f3f249232f44e538ce7485ab8b824445f2961cb5
Jason Pitteman <pittemanjason@hotmail.com>
2020-11-02 15:03:53 +0100

    Update StateMachine.py

--- OSD/StateMachine.py
+++ OSD/StateMachine.py
@@ -20,86 +20,86 @@ class StateMachine(object):
         # 7) Else -> inform the user that we are going to measure again
 
         
-    #global sensorWidth, sensorHeight, maxVal
+        #global sensorWidth, sensorHeight, maxVal
 
-    runningAvg = 0
-    thSampleCount = 0
-    thSampleAcc = []
-    thDataValid = False
-    nThSamplesToAverage = 1
-
-    gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-    rects = faceDet.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(180,180))
-    faceBoxes = [(y,x+w, y+h, x) for (x,y,w,h) in rects]
-    if len(faceBoxes) > 0:
-        tcFace1 = faceBoxes[0] # true color ROI
-        # transform the coordinates from true color image space to thermal image space using the affine transform matrix M
-        # See https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
-        M = np.array([[1.5689e-1, 8.6462e-3, -1.1660e+1],[1.0613e-4, 1.6609e-1, -1.4066e+1]])
-        P_slt = np.array([[tcFace1[3]],[tcFace1[0]],[1]]) # 'slt' source left top
-        P_srt = np.array([[tcFace1[1]],[tcFace1[0]],[1]]) # 'srt' source right top
-        P_slb = np.array([[tcFace1[3]],[tcFace1[2]],[1]]) # 'slb' source left bottom
-        P_srb = np.array([[tcFace1[1]],[tcFace1[2]],[1]]) # 'srb' source right bottom
-        P_dlt = np.dot(M, P_slt)
-        P_drt = np.dot(M, P_srt)
-        P_dlb = np.dot(M, P_slb)
-        P_drb = np.dot(M, P_srb)
-        thFace1Cnts = np.array([P_dlt, P_drt, P_dlb, P_drb], dtype=np.float32)
-        for (top,right,bottom,left) in faceBoxes:
-            cv.rectangle(image,(left,top),(right,bottom), (100,255,100), 1)
-
-        # get thermal image from Lepton
-        raw,_ = l.capture()
-        # find maximum value in raw lepton data array in thRoi (face1) slice of raw data.
-        thRoi = cv.boundingRect(thFace1Cnts)
-        x,y,w,h = thRoi
-        # x any shold not be negative. Clip the values.
-        x = max(0, min(x, sensorWidth))
-        y = max(0, min(y, sensorHeight))
-        thRoiData = raw[y:y+h, x:x+w]
-        maxVal = np.amax(thRoiData)
-        print("max val: " + str(maxVal) + "in deg: " + str(float(maxVal/100.0)-273.15))
-	    # get running average over N thermal samples
-        #if (thSampleCount < nThSamplesToAverage):
-        #    thSampleCount += 1
-        #    thSampleAcc.append(maxVal)
-        #else:
-        #    thDataValid = True
-        #    thSampleAcc.append(maxVal)
-        #    runningAvg = sum(thSampleAcc)/len(thSampleAcc)
-        #    print("running avg: " + str(runningAvg))
-        #    maxCoord = np.where(thRoiData == maxVal)
-    else:
-	    # No faces found.
         runningAvg = 0
         thSampleCount = 0
-        del thSampleAcc[:]
+        thSampleAcc = []
         thDataValid = False
+        nThSamplesToAverage = 1
 
-    if thDataValid:
-        # text position
-        txtPosition = (500,50)
-
-        cv.normalize(raw, raw, 0, 65535, cv.NORM_MINMAX) # extend contrast
-        np.right_shift(raw, 8, raw) # fit data into 8 bits
-        # draw roi if any
+        gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+        rects = faceDet.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(180,180))
+        faceBoxes = [(y,x+w, y+h, x) for (x,y,w,h) in rects]
         if len(faceBoxes) > 0:
-            x,y,w,h = thRoi
-            cv.rectangle(raw, (x,y), (x+w,y+h), 255, 1)
-        # make uint8 image
-        thermal = np.uint8(raw)
-        # convert grayscale to BGR
-        thermal = cv.cvtColor(thermal, cv.COLOR_GRAY2BGR)
-        color = image
+            tcFace1 = faceBoxes[0] # true color ROI
+            # transform the coordinates from true color image space to thermal image space using the affine transform matrix M
+            # See https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
+            M = np.array([[1.5689e-1, 8.6462e-3, -1.1660e+1],[1.0613e-4, 1.6609e-1, -1.4066e+1]])
+            P_slt = np.array([[tcFace1[3]],[tcFace1[0]],[1]]) # 'slt' source left top
+            P_srt = np.array([[tcFace1[1]],[tcFace1[0]],[1]]) # 'srt' source right top
+            P_slb = np.array([[tcFace1[3]],[tcFace1[2]],[1]]) # 'slb' source left bottom
+            P_srb = np.array([[tcFace1[1]],[tcFace1[2]],[1]]) # 'srb' source right bottom
+            P_dlt = np.dot(M, P_slt)
+            P_drt = np.dot(M, P_srt)
+            P_dlb = np.dot(M, P_slb)
+            P_drb = np.dot(M, P_srb)
+            thFace1Cnts = np.array([P_dlt, P_drt, P_dlb, P_drb], dtype=np.float32)
+            for (top,right,bottom,left) in faceBoxes:
+                cv.rectangle(image,(left,top),(right,bottom), (100,255,100), 1)
 
-    # Put data on top of the image if a face was detected.
-    if len(faceBoxes) == 1 and thDataValid:
-    #            measTemp = (float(maxVal/100.0)-273.15) + corrVal
-        measTemp = (float(runningAvg/100.0)-273.15)
-        if measTemp > feverThresh:
-            cv.putText(color, "{}degC".format(measTemp), txtPosition, cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255,255), 2)
+            # get thermal image from Lepton
+            raw,_ = l.capture()
+            # find maximum value in raw lepton data array in thRoi (face1) slice of raw data.
+            thRoi = cv.boundingRect(thFace1Cnts)
+            x,y,w,h = thRoi
+            # x any shold not be negative. Clip the values.
+            x = max(0, min(x, sensorWidth))
+            y = max(0, min(y, sensorHeight))
+            thRoiData = raw[y:y+h, x:x+w]
+            maxVal = np.amax(thRoiData)
+            print("max val: " + str(maxVal) + "in deg: " + str(float(maxVal/100.0)-273.15))
+	        # get running average over N thermal samples
+            #if (thSampleCount < nThSamplesToAverage):
+            #    thSampleCount += 1
+            #    thSampleAcc.append(maxVal)
+            #else:
+            #    thDataValid = True
+            #    thSampleAcc.append(maxVal)
+            #    runningAvg = sum(thSampleAcc)/len(thSampleAcc)
+            #    print("running avg: " + str(runningAvg))
+            #    maxCoord = np.where(thRoiData == maxVal)
         else:
-            cv.putText(color, "{}degC".format(measTemp), txtPosition, cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0,255),2)
+	        # No faces found.
+            runningAvg = 0
+            thSampleCount = 0
+            del thSampleAcc[:]
+            thDataValid = False
+
+        if thDataValid:
+            # text position
+            txtPosition = (500,50)
+
+            cv.normalize(raw, raw, 0, 65535, cv.NORM_MINMAX) # extend contrast
+            np.right_shift(raw, 8, raw) # fit data into 8 bits
+            # draw roi if any
+            if len(faceBoxes) > 0:
+                x,y,w,h = thRoi
+                cv.rectangle(raw, (x,y), (x+w,y+h), 255, 1)
+            # make uint8 image
+            thermal = np.uint8(raw)
+            # convert grayscale to BGR
+            thermal = cv.cvtColor(thermal, cv.COLOR_GRAY2BGR)
+            color = image
+
+        # Put data on top of the image if a face was detected.
+        if len(faceBoxes) == 1 and thDataValid:
+        #            measTemp = (float(maxVal/100.0)-273.15) + corrVal
+            measTemp = (float(runningAvg/100.0)-273.15)
+            if measTemp > feverThresh:
+                cv.putText(color, "{}degC".format(measTemp), txtPosition, cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255,255), 2)
+            else:
+                cv.putText(color, "{}degC".format(measTemp), txtPosition, cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0,255),2)
 
     def reset():
         print("Resetting state machine")
